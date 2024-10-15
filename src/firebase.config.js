1b4b8366ecbd91184e80178dad3dc94f1804fe28
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-10-15 21:30:10 +0200

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHpkp06igUMGSgbavYWhvTAaime8uPPNY",
+  authDomain: "profile-6f9f3.firebaseapp.com",
+  projectId: "profile-6f9f3",
+  storageBucket: "profile-6f9f3.appspot.com",
+  messagingSenderId: "757959704412",
+  appId: "1:757959704412:web:105783ea0dd2f0db580b99",
+  measurementId: "G-R875G8M871",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+// auth().useEmulator("http://localhost:5173/");
+
+auth.languageCode = "it";
